70e8ebbdb106189fba80be5d4233dc51e158af2d
Nikolai Holfelt Røssel <niko6307@edu.easj.dk>
2019-11-26 12:34:09 +0100

    Weather Station

---
+++ ModelLib/Forecast.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ModelLib
+{
+    public class Forecast
+    {
+        private double _temp;
+        private double _pressure;
+        private double _humidity;
+
+        public double Temp
+        {
+            get => _temp;
+            set => _temp = value;
+        }
+
+        public double Pressure
+        {
+            get => _pressure;
+            set => _pressure = value;
+        }
+
+        public double Humidity
+        {
+            get => _humidity;
+            set => _humidity = value;
+        }
+
+        public Forecast()
+        {
+            
+        }
+
+        public Forecast(double temp, double pressure, double humidity)
+        {
+            _temp = temp;
+            _pressure = pressure;
+            _humidity = humidity;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Temp)}: {Temp}, {nameof(Pressure)}: {Pressure}, {nameof(Humidity)}: {Humidity}";
+        }
+    }
+}
---
+++ WeatherStationRest/Controllers/WeathersController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelLib;
+
+namespace WeatherStationRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeathersController : ControllerBase
+    {
+        List<Forecast> _forecast = new List<Forecast>();
+
+        //[HttpGet]
+        //[Route("CurrentWeather")]
+        //public Forecast CurrentWeather()
+        //{
+
+        //}
+
+        // GET: api/Weathers
+        [HttpGet]
+        public IEnumerable<Forecast> Get()
+        {
+            return _forecast;
+        }
+
+        // GET: api/Weathers/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Weathers
+        [HttpPost]
+        public void Post([FromBody] Forecast forecast)
+        {
+            _forecast.Add(forecast);
+        }
+
+        // PUT: api/Weathers/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string forecast)
+        //{
+
+        //}
+
+        // DELETE: api/ApiWithActions/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
